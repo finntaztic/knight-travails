cce65b6ad1c8e1408a8740990e18f08d2da0326f
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-10-15 14:07:56 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function knightMoves(){ //shows the shortest possible way to get from one square to another
+    //output all the vertices knight will stop on along the way
+}
+//rule
+//chose search algorithm to find the shortest path 
+// between starting square and the ending square
+
+//knight basic move >> 
+// two step forward, and one side step (any direction)
+//one step forward an two steps to the side ( any direction)
