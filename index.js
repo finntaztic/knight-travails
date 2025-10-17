955091ff61d0a5d21bd4328dd9ee0354a4b233e5
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-10-17 10:52:51 +0800

    add functions

--- index.js
+++ index.js
@@ -4,6 +4,37 @@ function knightMoves(){
     //shows the shortest possible way to get from one square to another
     //output all the vertices knight will stop on along the way
 }
+
+//chessboard
+class Graph {
+    constructor(){
+        this.list = new Map();
+    }
+
+    //add vertex
+    addVertex (newVertex){
+        this.list.set(newVertex, []);
+    }
+
+    addEdge(vertex, edge){
+        this.list.get(vertex).push(edge);
+    }
+
+    printGraph(){
+        const vertices = this.list.keys();
+        for (let vertex of vertices){
+            const connectedNodes = this.list.get(vertex);
+            console.log(`${vertex} -> ${connectedNodes.join(', ')}`)
+        }
+        
+    }
+}
+
+const graph = new Graph();
+
+graph.addVertex('A');
+graph.addEdge('A', 'B');
+graph.printGraph();
 //rule
 //chose search algorithm to find the shortest path 
 // between starting square and the ending square
