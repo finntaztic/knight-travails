ed12d8448029425e2afb2028f4c8a84c3b6714d6
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-10-18 12:03:19 +0800

    update index.js

--- index.js
+++ index.js
@@ -5,36 +5,59 @@ function knightMoves(){
     //output all the vertices knight will stop on along the way
 }
 
-//chessboard
-class Graph {
-    constructor(){
-        this.list = new Map();
-    }
 
-    //add vertex
-    addVertex (newVertex){
-        this.list.set(newVertex, []);
-    }
-
-    addEdge(vertex, edge){
-        this.list.get(vertex).push(edge);
-    }
-
-    printGraph(){
-        const vertices = this.list.keys();
-        for (let vertex of vertices){
-            const connectedNodes = this.list.get(vertex);
-            console.log(`${vertex} -> ${connectedNodes.join(', ')}`)
-        }
-        
+class squarePos{
+    constructor(row, col){
+        this.row = row;
+        this.col = col;
     }
 }
 
-const graph = new Graph();
+//chessboard
+class Graph {
+    constructor(){
+        this.list = new Map();//it's like for each method,  
+        // accepts a callback and applies that function 
+        // to each element of an array, then return a new array
+    }
 
-graph.addVertex('A');
-graph.addEdge('A', 'B');
-graph.printGraph();
+    //make a graph
+    addVertex(){
+        let currSquare = new squarePos(4, 3);
+        console.log(currSquare)
+    }
+
+    addEdge(){
+        //make the move of the knight
+    }
+
+}
+
+const graph = new Graph();
+graph.addVertex()
+
+
+//     //add vertex
+//     addVertex (newVertex){
+//         this.list.set(newVertex, []);
+//     }
+
+//     addEdge(vertex, edge){
+//         this.list.get(vertex).push(edge);
+//     }
+
+//     printGraph(){
+//         const vertices = this.list.keys();
+//         for (let vertex of vertices){
+//             const connectedNodes = this.list.get(vertex);
+//             console.log(`${vertex} -> ${connectedNodes.join(', ')}`)
+//         }
+        
+//     }
+
+// graph.addVertex('A');
+// graph.addEdge('A', 'B');
+// graph.printGraph();
 //rule
 //chose search algorithm to find the shortest path 
 // between starting square and the ending square
