597503f79e88176c6bd71046540943939ca88b54
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-10-28 09:16:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,36 @@ class Graph {
 
     exploreMoves (curr){
 
-        console.log(curr)
-        if (this.list.has(curr)){
-                return;
-            } else this.list.set(curr, []);
+        const stringKey = curr.toString();
+        console.log(stringKey)
+
+        if (this.list.has(stringKey)){
+            return;
+        } else this.list.set(stringKey, [])
         
-        let edges = [[1, 2], [1, -2]]// [-1, -2], [-1, 2]] //, [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]];
+        let edges = [[1, 2], [1, -2], [-1, -2], [-1, 2],]//  [-1, 2]] //, [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]];
 
         edges.forEach((edge) => {
             let newRow = curr[0] + edge [0];
             let newCol = curr[1] + edge [1];
 
             if (newRow > 7 || newCol > 7 || newRow < 0 || newCol < 0) return;
+            if (newRow == curr [0] && newCol == curr [1]) return;
             let vertex = [newRow, newCol];
 
-            console.log(vertex)
-
             this.exploreMoves(vertex)
-            this.list.get(curr).push(vertex)
-
+            this.list.get(stringKey).push(vertex)
         })
-        }
     }
 
+    printGraph(){
+        const vertices = this.list.keys();
+        for (let vertex of vertices){
+            const connectedNodes = this.list.get(vertex);
+            console.log(`${vertex} -> ${connectedNodes.join(', ')}`)
+        }
+    }
+}
 
 
 
@@ -36,6 +43,7 @@ const graph = new Graph();
 console.log(graph.list);
 
 graph.exploreMoves([2, 3])
+graph.printGraph()
 // graph.addVertex([4,3])
 // graph.addEdge([4,3]);
 
